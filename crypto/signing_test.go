f2781c3ed80e03d160d2893a96f01e61bee1aff6
sebastian-garn <sebastian.garn@connctd.com>
2021-08-26 09:41:00 +0200

    Added test for empty body

--- crypto/signing_test.go
+++ crypto/signing_test.go
@@ -57,3 +57,20 @@ func TestSignatureCompositionWithEmptyBody(t *testing.T) {
 		t.Fatalf("Generated payload and expected payload do not match.\nExpctd: %v\n Given: %v", expected, string(toBeSigned))
 	}
 }
+
+func TestEmptyBody(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "https://foo.com:8080/bar?hello=world", nil)
+	require.NoError(t, err)
+
+	fakeTime := time.Date(2020, 10, 7, 10, 0, 0, 0, time.Now().UTC().Location())
+	req.Header.Set("Date", fakeTime.Format(http.TimeFormat))
+
+	toBeSigned, err := SignablePayload(req.Method, req.URL.Scheme, req.Host, req.URL.RequestURI(), req.Header, nil)
+	require.NoError(t, err)
+
+	expected := "(method):GET\r\n(url):https://foo.com:8080/bar?hello=world\r\n(Date):Wed, 07 Oct 2020 10:00:00 GMT\r\n(body):"
+
+	if string(toBeSigned) != expected {
+		assert.Equal(t, expected, string(toBeSigned))
+	}
+}
